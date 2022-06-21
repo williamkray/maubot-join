f316ccf59d26ae46f817b57cd5e4d9c71d20db33
William Kray <github@williamkray.com>
2022-06-21 12:23:12 -0700

    first pass at a super basic functionality bot

---
+++ README.md
@@ -0,0 +1,9 @@
+# join bot
+
+a really stupid simple maubot module that allows for only a designated list of users to get your maubot instance to join
+another room.
+
+the reason this exists is really because [this reature request](https://github.com/maubot/maubot/issues/110) exists but
+is not implemented.
+
+
---
+++ join/bot.py
@@ -0,0 +1,40 @@
+from typing import Awaitable, Type, Optional, Tuple
+import json
+import time
+
+from mautrix.client import Client
+from mautrix.types import (Event, StateEvent, EventID, UserID, FileInfo, EventType,
+                            RoomID, RoomAlias, ReactionEvent, RedactionEvent)
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
+from maubot import Plugin, MessageEvent
+from maubot.handlers import command, event
+
+
+class Config(BaseProxyConfig):
+    def do_update(self, helper: ConfigUpdateHelper) -> None:
+        helper.copy("admins")
+
+
+class Join(Plugin):
+
+    async def start(self) -> None:
+        await super().start()
+        self.config.load_and_update()
+        
+
+    @command.new("join", help="tell me a room to join and i'll do my scientific best")
+    @command.argument("room", required=True)
+    async def join_that_room(self, evt: MessageEvent, room: str) -> None:
+        if evt.sender in self.config["admins"]:
+            try:
+                await evt.respond(f"trying, give me a minute...")
+                await self.client.join_room(room, max_retries=3)
+                await evt.respond(f"i'm in!")
+            except Exception as e:
+                await evt.respond(f"i tried, but couldn't join because \"{e}\"")
+        else:
+            await evt.reply("you're not the boss of me!")
+
+    @classmethod
+    def get_config_class(cls) -> Type[BaseProxyConfig]:
+        return Config
